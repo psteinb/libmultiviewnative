a1164fc9d222acf3fd8e2f06ee39ed638cb94e45
Peter Steinbach <steinbac@mpi-cbg.de>
2013-12-13 17:25:04 +0100

    cleaned up header file and added libcuda.so to the linking process during build

--- inc/multiviewnative.h
+++ inc/multiviewnative.h
@@ -4,27 +4,27 @@
 typedef float imageType;
 
 #ifdef _WIN32
-#define FUNCTION_HOOK __declspec(dllexport)
+#define FUNCTION_PREFIX extern "C" __declspec(dllexport)
 #else
-#define FUNCTION_HOOK
+#define FUNCTION_PREFIX extern "C"
 #endif
 
 //--------------------------------------------- Win -------------------------------------------
 //#ifdef _WIN32
-FUNCTION_HOOK void convolution3DfftCUDAInPlace(imageType* im,int* imDim,imageType* kernel,int* kernelDim,int devCUDA);
-FUNCTION_HOOK void convolution3DfftCUDAInPlace_core(imageType* _d_imCUDA,int* imDim,
+FUNCTION_PREFIX void convolution3DfftCUDAInPlace(imageType* im,int* imDim,imageType* kernel,int* kernelDim,int devCUDA);
+FUNCTION_PREFIX void convolution3DfftCUDAInPlace_core(imageType* _d_imCUDA,int* imDim,
 							  imageType* _d_kernelCUDA,int* kernelDim,
 							  int devCUDA);
-FUNCTION_HOOK void compute_quotient(imageType* _input,imageType* _output,size_t _size, int _device);
-FUNCTION_HOOK void compute_final_values(imageType* _image,imageType* _integral,imageType* _weight,size_t _size, float _minValue, double _lambda, int _device);
-FUNCTION_HOOK void iterate_fft_plain(imageType* _input,
+FUNCTION_PREFIX void compute_quotient(imageType* _input,imageType* _output,size_t _size, int _device);
+FUNCTION_PREFIX void compute_final_values(imageType* _image,imageType* _integral,imageType* _weight,size_t _size, float _minValue, double _lambda, int _device);
+FUNCTION_PREFIX void iterate_fft_plain(imageType* _input,
 				     imageType* _kernel,
 				     imageType* _output,
 				     int* _input_dims,
 				     int* _kernel_dims, 
 				     int _device);
 
-FUNCTION_HOOK void iterate_fft_tikhonov(imageType* _input,
+FUNCTION_PREFIX void iterate_fft_tikhonov(imageType* _input,
 					imageType* _kernel,
 					imageType* _output,
 					int* _input_dims,
@@ -34,16 +34,16 @@ FUNCTION_HOOK void iterate_fft_tikhonov(imageType* _input,
 					double _lambda, 
 					int _device);
 
-FUNCTION_HOOK int selectDeviceWithHighestComputeCapability();
-FUNCTION_HOOK int getCUDAcomputeCapabilityMinorVersion(int devCUDA);
-FUNCTION_HOOK int getCUDAcomputeCapabilityMajorVersion(int devCUDA);
-FUNCTION_HOOK int getNumDevicesCUDA();
-FUNCTION_HOOK void getNameDeviceCUDA(int devCUDA, char *name);
-FUNCTION_HOOK long long int getMemDeviceCUDA(int devCUDA);
+FUNCTION_PREFIX int selectDeviceWithHighestComputeCapability();
+FUNCTION_PREFIX int getCUDAcomputeCapabilityMinorVersion(int devCUDA);
+FUNCTION_PREFIX int getCUDAcomputeCapabilityMajorVersion(int devCUDA);
+FUNCTION_PREFIX int getNumDevicesCUDA();
+FUNCTION_PREFIX void getNameDeviceCUDA(int devCUDA, char *name);
+FUNCTION_PREFIX long long int getMemDeviceCUDA(int devCUDA);
 
-#ifdef __CUDA_RUNTIME_H__
-FUNCTION_HOOK void fit_2Dblocks_to_threads_for_device(dim3& _blocks, dim3& _threads, const int& _device);
-#endif
+// #ifdef __CUDA_RUNTIME_H__
+// FUNCTION_PREFIX void fit_2Dblocks_to_threads_for_device(dim3& _blocks, dim3& _threads, const int& _device);
+// #endif
 
 // //--------------------------------------------- Linux/OSX -------------------------------------------
 // #else
