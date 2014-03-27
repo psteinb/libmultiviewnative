e1835e5dd6d4a35cf8328b371fa6fb7eefe7ba50
Peter Steinbach <steinbach@scionics.de>
2014-03-27 17:22:06 +0100

    fixed ugly memory corruption in test fixtures, moved entirely to boost::multi_array

--- tests/test_Independent.cpp
+++ tests/test_Independent.cpp
@@ -1,36 +1,28 @@
 #define BOOST_TEST_DYN_LINK 
 #define BOOST_TEST_MODULE Independent
 #include "boost/test/unit_test.hpp"
+#include "test_fixtures.hpp"
 
-
-class Independent_fixture
-{
-public:
-
-  size_t evenDim;
-
-  Independent_fixture():
-    evenDim(42)
-  {
-    BOOST_MESSAGE( "setup fixture" );
-  }
-  
-  virtual ~Independent_fixture()  { BOOST_MESSAGE( "teardown fixture" ); };
-   
-};
  
-BOOST_FIXTURE_TEST_SUITE( Independent_suite, Independent_fixture )
+BOOST_FIXTURE_TEST_SUITE( access_test_suite, multiviewnative::default_3D_fixture )
    
-BOOST_AUTO_TEST_CASE( fail )
+
+BOOST_AUTO_TEST_CASE( first_value )
 {
   
-  BOOST_CHECK(evenDim == 43);
+  BOOST_CHECK(image_[0][0][0] == 42.f);
 }
 
-BOOST_AUTO_TEST_CASE( success )
+BOOST_AUTO_TEST_CASE( center_value )
 {
   
-  BOOST_CHECK(evenDim == 42);
+  BOOST_CHECK(image_[0][0][0] == 42.f);//?
 }
+
+// BOOST_AUTO_TEST_CASE( axis_length )
+// {
+  
+//   BOOST_CHECK(image_.shape()[0] == image_.shape()[1] == image_.shape()[2] == 16 );
+// }
 
 BOOST_AUTO_TEST_SUITE_END()
