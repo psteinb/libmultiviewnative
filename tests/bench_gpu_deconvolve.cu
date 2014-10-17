448480b2803b3e0d1f5480b7ef2ddd9db9be4395
Peter Steinbach <steinbach@scionics.de>
2014-10-17 16:52:20 +0200

    rearranged setup of bench_gpu_deconvolve to not run into OpenMP issues observed with nvcc

--- tests/bench_gpu_deconvolve.cu
+++ tests/bench_gpu_deconvolve.cu
@@ -10,6 +10,7 @@
 #include "test_fixtures.hpp"
 #include "image_stack_utils.h"
 #include "multiviewnative.h"
+#include "cpu_kernels.h"
 
 #include <boost/timer/timer.hpp>
 
@@ -24,7 +25,7 @@ void split(const std::string& _s, std::vector<int>& _tgt){
   boost::sregex_token_iterator i(_s.begin(), _s.end(), re, -1);
   boost::sregex_token_iterator j;
 
-  int exp_numbers = std::count(_s.begin(), _s.end(), 'x') + 1;
+  unsigned exp_numbers = std::count(_s.begin(), _s.end(), 'x') + 1;
   if(_tgt.size()< exp_numbers)
     _tgt.resize(exp_numbers);
   
@@ -54,68 +55,103 @@ void split(const std::string& _s, std::vector<int>& _tgt){
 #endif
 #endif
 
-  struct simulated_data {
+struct simulated_data {
 
-    std::vector<multiviewnative::image_stack> views_;
-    std::vector<multiviewnative::image_stack> kernel1_;
-    std::vector<multiviewnative::image_stack> kernel2_;
-    std::vector<multiviewnative::image_stack> weights_;
+  std::vector<multiviewnative::image_stack> views_;
+  std::vector<multiviewnative::image_stack> kernel1_;
+  std::vector<multiviewnative::image_stack> kernel2_;
+  std::vector<multiviewnative::image_stack> weights_;
     
-    std::vector<int> stack_dims_;
-    std::vector<int> kernel1_dims_;
-    std::vector<int> kernel2_dims_;
+  std::vector<int> stack_dims_;
+  std::vector<int> kernel1_dims_;
+  std::vector<int> kernel2_dims_;
 
-    CUDA_HOST_PREFIX simulated_data(const std::vector<int>& stack_dims, int _n_views = 6):
-      views_(_n_views),
-      kernel1_(_n_views),
-      kernel2_(_n_views),
-      weights_(_n_views),
-      stack_dims_(stack_dims),
-      kernel1_dims_(stack_dims),
-      kernel2_dims_(stack_dims)
-    {
+  CUDA_HOST_PREFIX simulated_data(const std::vector<int>& stack_dims, int _n_views = 6):
+    views_(_n_views),
+    kernel1_(_n_views),
+    kernel2_(_n_views),
+    weights_(_n_views),
+    stack_dims_(stack_dims),
+    kernel1_dims_(stack_dims),
+    kernel2_dims_(stack_dims)
+  {
       
       
-      for(unsigned d = 0;d < stack_dims.size(); ++d){
-	kernel1_dims_[d] *= .05*(d+1);
-	kernel2_dims_[d] *= .02*(d+1);
-      }
+    for(unsigned d = 0;d < stack_dims.size(); ++d){
+      kernel1_dims_[d] *= .05*(d+1);
+      kernel2_dims_[d] *= .02*(d+1);
+    }
 
-      unsigned int num_elements = std::accumulate(stack_dims.begin(), stack_dims.end(), 1, std::multiplies<int>());
+    unsigned int num_elements = std::accumulate(stack_dims.begin(), stack_dims.end(), 1, std::multiplies<int>());
 
-      for(int i = 0; i < _n_views; ++i){
+    for(int i = 0; i < _n_views; ++i){
 	
 	
-	views_[i].resize(boost::extents[stack_dims_[0]][stack_dims_[1]][stack_dims_[2]]);
-	weights_[i].resize(boost::extents[stack_dims_[0]][stack_dims_[1]][stack_dims_[2]]);
-	std::fill(weights_[i].data(), weights_[i].data() + num_elements, 1.f);
-	std::fill(views_[i].data(), views_[i].data() + num_elements, 16.f + 4.*i);
+      views_[i].resize(boost::extents[stack_dims_[0]][stack_dims_[1]][stack_dims_[2]]);
+      weights_[i].resize(boost::extents[stack_dims_[0]][stack_dims_[1]][stack_dims_[2]]);
+      std::fill(weights_[i].data(), weights_[i].data() + num_elements, 1.f);
+      std::fill(views_[i].data(), views_[i].data() + num_elements, 16.f + 4.*i);
 	
-	kernel1_[i].resize(boost::extents[kernel1_dims_[0]][kernel1_dims_[1]][kernel1_dims_[2]]);
-	kernel1_[i][kernel1_dims_[0]/2][kernel1_dims_[1]/2][kernel1_dims_[2]/2] = i+1;
-	kernel2_[i].resize(boost::extents[kernel2_dims_[0]][kernel2_dims_[1]][kernel2_dims_[2]]);
-	kernel2_[i][kernel2_dims_[0]/2][kernel2_dims_[1]/2][kernel2_dims_[2]/2] = i+2;
-
-      }
+      kernel1_[i].resize(boost::extents[kernel1_dims_[0]][kernel1_dims_[1]][kernel1_dims_[2]]);
+      kernel1_[i][kernel1_dims_[0]/2][kernel1_dims_[1]/2][kernel1_dims_[2]/2] = i+1;
+      kernel2_[i].resize(boost::extents[kernel2_dims_[0]][kernel2_dims_[1]][kernel2_dims_[2]]);
+      kernel2_[i][kernel2_dims_[0]/2][kernel2_dims_[1]/2][kernel2_dims_[2]/2] = i+2;
 
     }
 
-    void info() {
-      int nviews = views_.size();
-      std::cout << "[simulated_data] " 
-		<< nviews << "x views/weights "<< views_[0].shape()[0] << "x"  << views_[0].shape()[1] << "x"  << views_[0].shape()[2] 
-		<< " kernel1 "<< kernel1_[0].shape()[0] << "x"  << kernel1_[0].shape()[1] << "x"  << kernel1_[0].shape()[2] 
-		<< " kernel2 "<< kernel2_[0].shape()[0] << "x"  << kernel2_[0].shape()[1] << "x"  << kernel2_[0].shape()[2] 
-		<< "\n";
+  }
 
-    }
+  void info() {
+    int nviews = views_.size();
+    std::cout << "[simulated_data] " 
+	      << nviews << "x views/weights "<< views_[0].shape()[0] << "x"  << views_[0].shape()[1] << "x"  << views_[0].shape()[2] 
+	      << " kernel1 "<< kernel1_[0].shape()[0] << "x"  << kernel1_[0].shape()[1] << "x"  << kernel1_[0].shape()[2] 
+	      << " kernel2 "<< kernel2_[0].shape()[0] << "x"  << kernel2_[0].shape()[1] << "x"  << kernel2_[0].shape()[2] 
+	      << "\n";
+
+  }
 };
+
+void default_inplace_gpu_deconvolve(float* _psi, workspace* _ws, int device = -1){
+
+
+  unsigned long stack_size = _ws->data_[0].image_dims_[0]*_ws->data_[0].image_dims_[1]*_ws->data_[0].image_dims_[2];
+  std::vector<float> integral(stack_size);
+
+  if(device < 0)
+    device = selectDeviceWithHighestComputeCapability();
+
+  for(int it = 0;it<_ws->num_iterations_;++it){
+
+    for(int v = 0;v < _ws->num_views_;++v){
+      
+      std::copy(_psi, _psi + stack_size, integral.begin());
+      
+      convolution3DfftCUDAInPlace(&integral[0], _ws->data_[v].image_dims_,
+				  _ws->data_[v].kernel1_,_ws->data_[v].kernel1_dims_,device);
+      
+      //serial
+      computeQuotient(_ws->data_[v].image_, &integral[0], stack_size);
+
+      convolution3DfftCUDAInPlace(&integral[0], _ws->data_[v].image_dims_,
+				  _ws->data_[v].kernel2_,_ws->data_[v].kernel2_dims_,device);
+      //serial
+      serial_regularized_final_values(_psi, &integral[0], _ws->data_[v].weights_, 
+				      stack_size,
+				      _ws->lambda_ ,
+				      _ws->minValue_ );
+    }
+    
+  }
+
+}
 
 
 int main(int argc, char *argv[])
 {
 
   bool verbose = false;
+  bool use_default = false;
   int num_views = 6;
   int num_iterations = 5;
   int num_repeats = 5;
@@ -125,6 +161,7 @@ int main(int argc, char *argv[])
   desc.add_options()
     ("help,h", "produce help message")
     ("verbose,v", "print lots of information in between")
+    ("use_default,d", "use implementation used in the original SPIM_Registration plugin ")
     ("stack_dimensions,s", po::value<std::string>(&stack_dims)->default_value("512x512x64"), "HxWxD of synthetic stacks to generate")
     ("views,n", po::value<int>(&num_views)->default_value(6), "number of views to generate")
     ("iterations,i", po::value<int>(&num_iterations)->default_value(5), "location of kernel image")
@@ -144,6 +181,8 @@ int main(int argc, char *argv[])
   }
 
   verbose = vm.count("verbose");
+  use_default = vm.count("use_default");
+
   std::vector<int> numeric_stack_dims(3);
   numeric_stack_dims[0] = 512;
   numeric_stack_dims[1] = 512;
@@ -179,11 +218,23 @@ int main(int argc, char *argv[])
   double time_ms = 0.f;
   
   for(int r = 0;r<num_repeats;++r){
-    if(verbose)
-      std::cout << "[inplace_gpu_deconvolve] repeat " << r << "/" << num_repeats;
+    
+    
+    if(use_default){
+      if(verbose)
+	std::cout << "[default inplace_gpu_deconvolve] repeat " << r << "/" << num_repeats;
+      cpu_timer timer;
+      default_inplace_gpu_deconvolve(&psi[0], &input, -1);
+      durations[r] = timer.elapsed();
+    }
+    else{
+      if(verbose)
+	std::cout << "[inplace_gpu_deconvolve] repeat " << r << "/" << num_repeats;
     cpu_timer timer;
     inplace_gpu_deconvolve(&psi[0], input, -1);
     durations[r] = timer.elapsed();
+    }
+
     time_ms += double(durations[r].system + durations[r].user)/1e6;
     if(verbose){
       std::cout << " took " << double(durations[r].system + durations[r].user)/1e6 << " ms\n";
